d55f16e982f8e377a8a99d5d9e23710b962a3cbe
Guilherme Guerreiro <guiguerreiro10@hotmail.com>
2020-09-30 20:25:42 +0100

    netlify dashboard and hook

---
+++ src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+    widgets: [
+        {
+            name: "netlify",
+            options: {
+                title: "Netlify deploy",
+                sites: [
+                    {
+                        title: "Portfolio Website",
+                        apiId: "92d51190-91ba-4c4b-ae1e-6a29805e6de2",
+                        buildHookId: "5f74d848f8be94012820205f",
+                        name: "guilherme-guerreiro",
+                    },
+                ],
+            },
+        },
+    ],
+};
